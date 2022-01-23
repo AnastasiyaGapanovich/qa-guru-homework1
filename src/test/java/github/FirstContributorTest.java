3310527497c1c99ebccaf5ff5d9a6eeb79015529
AnastasiyaGapanovich <baytsan@yandex.ru>
2022-01-23 19:22:13 +0300

    commit nameTest

--- src/test/java/github/FirstContributorTest.java
+++ src/test/java/github/FirstContributorTest.java
@@ -7,7 +7,7 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class FirstContributorTest {
     @Test
-    void firstContributerShouldBeAndreiSolntsev(){
+    void firstContributorShouldBeAndreiSolntsev(){
         open("https://github.com/selenide/selenide");
       //  $$("ul.list-style-none.d-flex.flex-wrap.mb-n2 li").first().hover();
    // $$(".Popover-message").findBy(Condition.visible).shouldHave(Condition.text("Andrei Solntsev"));
