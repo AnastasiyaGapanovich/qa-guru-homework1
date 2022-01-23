dd42a8e3b835ad9750057d25f87c07e7faed3e91
AnastasiyaGapanovich <baytsan@yandex.ru>
2022-01-23 19:31:07 +0300

    commit alignment

--- src/test/java/github/FirstContributorTest.java
+++ src/test/java/github/FirstContributorTest.java
@@ -7,13 +7,11 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class FirstContributorTest {
     @Test
-    void firstContributorShouldBeAndreiSolntsev(){
+    void firstContributorShouldBeAndreiSolntsev() {
         open("https://github.com/selenide/selenide");
-      //  $$("ul.list-style-none.d-flex.flex-wrap.mb-n2 li").first().hover();
-   // $$(".Popover-message").findBy(Condition.visible).shouldHave(Condition.text("Andrei Solntsev"));
-$(".Layout-sidebar").$$("ul li").first().hover();
-$$(".Popover-message").findBy(Condition.visible).shouldHave(Condition.text("Andrei Solntsev"));
-        sleep(5000);
-
+        // $$("ul.list-style-none.d-flex.flex-wrap.mb-n2 li").first().hover();
+        // $$(".Popover-message").findBy(Condition.visible).shouldHave(Condition.text("Andrei Solntsev"));
+        $(".Layout-sidebar").$$("ul li").first().hover();
+        $$(".Popover-message").findBy(Condition.visible).shouldHave(Condition.text("Andrei Solntsev"));
     }
 }
